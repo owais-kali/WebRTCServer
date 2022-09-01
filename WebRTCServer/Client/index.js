12ac2eb4cc6b2183d686ae0e352a352403511280
sh.owais95@gmail.com <sh.owais95@gmail.com>
2022-09-01 08:21:50 +0530

    Create basic WebRTC page

---
+++ WebRTCServer/Client/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path');
+
+const app = express()
+const port = 3000
+
+const rootPath = "/views"
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/example1/index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
